6750d97c169995d2554cb68b8df68f8d8e756c86
Nina Bornemann <nina.bornemann@freenet.de>
2025-09-18 01:29:11 -0700

    Initial commit

---
+++ src/main/java/com/ninabornemann/rickandmorty_externalapi/RickAndMortyExternalApiApplication.java
@@ -0,0 +1,13 @@
+package com.ninabornemann.rickandmorty_externalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyExternalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndMortyExternalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ninabornemann/rickandmorty_externalapi/RickAndMortyExternalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninabornemann.rickandmorty_externalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyExternalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
